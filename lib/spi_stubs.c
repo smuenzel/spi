67232b8547368d213f51559815e75f2fba48d4e5
Stefan Muenzel <source@s.muenzel.net>
2025-04-02 09:19:15 +0200

    Error

--- lib/spi_stubs.c
+++ lib/spi_stubs.c
@@ -1,32 +1,30 @@
 #include <sys/ioctl.h>
+#include <errno.h>
 #include <linux/spi/spidev.h>
 
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 #include <caml/bigarray.h>
+#include <caml/unixsupport.h>
 
 /* CR smuenzel: need to figure out a way to include iobuf.h from core_unix.iobuf_unix */
 enum iobuf_fields { iobuf_buf, iobuf_lo_min, iobuf_lo, iobuf_hi, iobuf_hi_max };
 
-CAMLprim value spi_transfer_stub(value v_fd, value v_transfer_array)
-{
+CAMLprim value spi_transfer_stub(value v_fd, value v_transfer_array){
   CAMLparam1(v_transfer_array);
   size_t number_of_transfers = Wosize_val(v_transfer_array);
   int fd = Int_val(v_fd);
-  if(number_of_transfers == 0)
-  {
+  if(number_of_transfers == 0){
     CAMLreturn(Val_int(0));
   }
 
   struct spi_ioc_transfer transfers[number_of_transfers] = {};
 
-  for(size_t i = 0; i < number_of_transfers; i++)
-  {
+  for(size_t i = 0; i < number_of_transfers; i++){
     value v_entry = Field(v_transfer_array, i);
 
     value v_tx_buf_opt = Field(v_entry, 0); 
-    if(Is_some(v_tx_buf_opt))
-    {
+    if(Is_some(v_tx_buf_opt)){
       value v_tx_buf = Some_val(v_tx_buf_opt);
       value v_tx_buf_bs = Field(v_tx_buf, iobuf_buf);
       ptrdiff_t tx_buf_start = Long_val(Field(v_tx_buf, iobuf_lo));
@@ -36,8 +34,7 @@ CAMLprim value spi_transfer_stub(value v_fd, value v_transfer_array)
     }
 
     value v_rx_buf_opt = Field(v_entry, 1); 
-    if(Is_some(v_rx_buf_opt))
-    {
+    if(Is_some(v_rx_buf_opt)){
       value v_rx_buf = Some_val(v_rx_buf_opt);
       value v_rx_buf_bs = Field(v_rx_buf, iobuf_buf);
       ptrdiff_t rx_buf_start = Long_val(Field(v_rx_buf, iobuf_lo));
@@ -59,7 +56,10 @@ CAMLprim value spi_transfer_stub(value v_fd, value v_transfer_array)
   caml_enter_blocking_section();
   int status = ioctl(fd, SPI_IOC_MESSAGE(number_of_transfers), transfers);
   caml_leave_blocking_section();
+
+  if(status == -1){
+    caml_unix_error(errno, "spi_transfer", Nothing);
+  }
   
   CAMLreturn(Val_int(status));
-
 }
